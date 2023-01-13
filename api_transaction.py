560962ce8b0a7ea711dea79b4c9e7b4dd0fe92a2
Allan Coutarel <allancoutarel@orange.fr>
2023-01-14 00:46:43 +0100

    clean code and rewrite it in english

--- api_transaction.py
+++ api_transaction.py
@@ -11,47 +11,51 @@ transactions = []
 persons = [] 
 
 class Person:
-    def __init__(self, nom, prenom, solde):
-        self.nom = nom
-        self.prenom = prenom
-        self.solde = solde
+    def __init__(self, lastname, firstname, account):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.account = account
     
     def to_json(self):
-        return {'nom': self.nom, 'prenom': self.prenom, 'solde': self.solde}
+        return {'lastname': self.lastname, 'firstname': self.firstname, 'account': self.account}
 
     def to_json_identity(self):
-        return {'nom': self.nom, 'prenom': self.prenom}
+        return {'lastname': self.lastname, 'firstname': self.firstname}
         
     def debit(self, sum):
-        self.solde -= sum
+        self.account -= sum
         
     def credit(self, sum):
-        self.solde += sum
+        self.account += sum
         
     def transaction(self, P2, sum):
         self.debit(sum)
         P2.credit(sum)
     
-    def equals(self, nom, prenom):
-        return nom == self.nom and prenom == self.prenom
+    def equals(self, lastname, firstname):
+        return lastname == self.lastname and firstname == self.firstname
 
-def find(nom, prenom):
-    for personne in persons:
-        if(personne.equals(nom, prenom)):
-            return personne
+def find(lastname, firstname):
+    for person in persons:
+        if(person.equals(lastname, firstname)):
+            return person
     return False
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def print():
-    res = "<h1>Liste des personnes :</h1><ul>"
-    for person in persons:
-        res += "<li>NOM : " + person.nom + " / PRENOM : " + person.prenom + " / SOLDE COMPTE : " + '%.2f' % person.solde + "</li>"
-    res += "</ul><h1>Liste des transactions :</h1><ul>"
-    for transaction in transactions:
-        res += "<li>P1 : " + transaction[0].nom + " " + transaction[0].prenom + " / P2 : " + transaction[1].nom + " " + transaction[1].prenom + " / DATE : " + transaction[2].strftime("%Y-%m-%d %H:%M:%S") + " / SOMME : " + '%.2f' % transaction[3] + "</li>"
-    return res+"</ul>"
+    if request.method == 'GET':
+        res = "<h1>Liste des personnes :</h1><ul>"
+        for person in persons:
+            res += "<li>NOM : " + person.lastname + " / PRENOM : " + person.firstname + " / SOLDE COMPTE : " + '%.2f' % person.account + "€</li>"
+        res += "</ul><h1>Liste des transactions :</h1><ul>"
+        for transaction in transactions:
+            res += "<li>P1 : " + transaction[0].lastname + " " + transaction[0].firstname + " / P2 : " + transaction[1].lastname + " " + transaction[1].firstname + " / DATE : " + transaction[2].strftime("%Y-%m-%d %H:%M:%S") + " / SOMME : " + '%.2f' % transaction[3] + "€</li>"
+        return res+"</ul>"
+    else:
+        return "Invalid request method"
 
+# http://localhost:5000/
 
 @app.route('/import', methods=['POST'])
 def import_data():
@@ -66,6 +70,8 @@ def import_data():
             persons.append(Person(row[0], row[1], float(row[2])))
         
         return jsonify("CSV file imported with success !")
+    else:
+        return "Invalid request method"
 
 # curl -X POST -F 'file=@persons.csv' http://localhost:5000/import
 
@@ -74,30 +80,46 @@ def import_data():
 def print_transactions():
     if request.method == 'GET':
         sorted_transactions = sorted(transactions, key=lambda transaction: transaction[2])
-        return jsonify([{'P1': person1.to_json_identity(), 'P2': person2.to_json_identity(), 't': t, 's': s} for person1, person2, t, s in sorted_transactions])
+        res = "<h1>Liste des transactions triées par odre chronologique :</h1><ul>"
+        for transaction in sorted_transactions:
+            res += "<li>P1 : " + transaction[0].lastname + " " + transaction[0].firstname + " / P2 : " + transaction[1].lastname + " " + transaction[1].firstname + " / DATE : " + transaction[2].strftime("%Y-%m-%d %H:%M:%S") + " / SOMME : " + '%.2f' % transaction[3] + "€</li>"
+        return res+"</ul>"
+    else:
+        return "Invalid request method"
 
-# curl -X "GET" http://localhost:5000/print-transactions
+# curl -X GET "http://localhost:5000/print-transactions"
+# In navigator : http://localhost:5000/print-transactions
 
 
 @app.route('/print-person-transactions', methods=['GET'])
-def print_person_transactions(): 
+def print_person_transactions():
     if request.method == 'GET':
-        person_transactions = []
-        nom = request.form['nom']
-        prenom = request.form['prenom']
+        lastname = request.args.get('lastname')
+        firstname = request.args.get('firstname')
         
-        person1 = find(nom, prenom)
+        person = find(lastname, firstname)
         
-        for transaction in transactions:
-            if transaction[0].nom == person1.nom and transaction[0].prenom == person1.prenom:
-                person_transactions.append(transaction)
-            elif transaction[1].nom == person1.nom and transaction[1].prenom == person1.prenom:
-                person_transactions.append(transaction)
+        if person != False:
+            person_transactions = []
             
-        sorted_person_transactions = sorted(person_transactions, key=lambda transaction: transaction[2])
-        return jsonify([{'P1': person1.to_json_identity(), 'P2': person2.to_json_identity(), 't': t, 's': s} for person1, person2, t, s in sorted_person_transactions])
+            for transaction in transactions:
+                if transaction[0].lastname == person.lastname and transaction[0].firstname == person.firstname:
+                    person_transactions.append(transaction)
+                elif transaction[1].lastname == person.lastname and transaction[1].firstname == person.firstname:
+                    person_transactions.append(transaction)
+            
+            sorted_person_transactions = sorted(person_transactions, key=lambda transaction: transaction[2])
+            res = "<h1>Liste des transactions triées par odre chronologique liées à " + person.lastname + " " + person.firstname + " :</h1><ul>"
+            for transaction in sorted_person_transactions:
+                res += "<li>P1 : " + transaction[0].lastname + " " + transaction[0].firstname + " / P2 : " + transaction[1].lastname + " " + transaction[1].firstname + " / DATE : " + transaction[2].strftime("%Y-%m-%d %H:%M:%S") + " / SOMME : " + '%.2f' % transaction[3] + "€</li>"
+            return res+"</ul>"
+        else:
+            return "Invalid request, need valid lastname and firstname"
+    else:
+        return "Invalid request method"
 
-# curl -X "GET" -d "nom=Dupont&prenom=Jean" http://localhost:5000/print-person-transactions
+# curl -X GET "http://localhost:5000/print-person-transactions?lastname=Dupont&firstname=Jean"
+# In navigator : http://localhost:5000/print-person-transactions?lastname=Dupont&firstname=Jean
 
 
 @app.route("/do-transaction", methods=['PUT'])
@@ -106,38 +128,46 @@ def do_transaction():
         data = request.get_json()
         
         P1 = data['P1']
-        person1 = find(P1['nom'], P1['prenom'])
+        person1 = find(P1['lastname'], P1['firstname'])
             
         P2 = data['P2']
-        person2 = find(P2['nom'], P2['prenom'])
+        person2 = find(P2['lastname'], P2['firstname'])
         
-        t = datetime.strptime(data['t'], "%Y-%m-%d %H:%M:%S")
-        s = data['s']
+        if person1 != False and person2 != False:
         
-        transactions.append((person1, person2, t, s))
-        transactions_json = {'P1': person1.to_json(), 'P2': person2.to_json(), 't': t, 's': s}
-        person1.transaction(person2, s)
-
-        return jsonify(transactions_json)
-
-# curl -X PUT -H "Content-Type: application/json" -d '{"P1": {"nom": "Dupont", "prenom": "Jean"}, "P2": {"nom": "Burger", "prenom": "Dylan"}, "t": "2023-01-12 15:04:22", "s": 50}' http://localhost:5000/do-transaction
-# curl -X PUT -H "Content-Type: application/json" -d '{"P1": {"nom": "Burger", "prenom": "Dylan"}, "P2": {"nom": "Dupont", "prenom": "Jean"}, "t": "2023-01-12 17:10:52", "s": 20}' http://localhost:5000/do-transaction
-
-@app.route('/affiche-solde', methods=['GET'])
-def affiche_solde():
-    if request.method == 'GET':
-        nom = request.form['nom']
-        prenom = request.form['prenom']
-        
-        person = find(nom, prenom)
-        if(person == False):
-            solde = "This person doesn't exist"
+            t = datetime.strptime(data['t'], "%Y-%m-%d %H:%M:%S")
+            s = data['s']
+            
+            transactions.append((person1, person2, t, s))
+            transactions_json = {'P1': person1.to_json(), 'P2': person2.to_json(), 't': t, 's': s}
+            person1.transaction(person2, s)
+            return jsonify(transactions_json)
         else:
-            solde = person.solde
+            return "Invalid request, need valid persons"
+    else:
+        return "Invalid request method"
 
-        return jsonify("solde : " + str(solde))
+# curl -X PUT -H "Content-Type: application/json" -d '{"P1": {"lastname": "Dupont", "firstname": "Jean"}, "P2": {"lastname": "Burger", "firstname": "Dylan"}, "t": "2023-01-12 15:04:22", "s": 50}' http://localhost:5000/do-transaction
+# curl -X PUT -H "Content-Type: application/json" -d '{"P1": {"lastname": "Burger", "firstname": "Dylan"}, "P2": {"lastname": "Dupont", "firstname": "Jean"}, "t": "2023-01-12 17:10:52", "s": 20}' http://localhost:5000/do-transaction
 
-# curl -X "GET" -d "nom=Dupont&prenom=Jean" http://localhost:5000/affiche-solde
+@app.route('/print-account', methods=['GET'])
+def print_account():
+    if request.method == 'GET':
+        lastname = request.args.get('lastname')
+        firstname = request.args.get('firstname')
+        
+        person = find(lastname, firstname)
+        
+        if person != False:
+            account = person.account
+            return "<p>Solde de " + person.lastname + " " + person.firstname + " : " + str(account) + "€</p>"
+        else:
+            return "Invalid request, need valid lastname and firstname"
+    else:
+        return "Invalid request method"
+
+# curl -X GET "http://localhost:5000/print-account?lastname=Dupont&firstname=Jean"
+# In navigator : http://localhost:5000/print-account?lastname=Dupont&firstname=Jean
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 :
